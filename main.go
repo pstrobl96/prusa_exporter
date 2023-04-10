94489144b38af9cff42beabce658fc7faea38237
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2023-04-10 14:36:21 +0200

    created einsy dashboard

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	log.Println("Buddy Prusa Link Prometheus exporter starting")
 	loadEnvVars()
+	//snap, err := getSnap("192.168.20.146")
 	buddyCollector := newBuddyCollector()
 	legacyCollector := newLegacyCollector()
 	einsyCollector := newEinsyCollector()
@@ -19,5 +20,7 @@ func main() {
 	log.Println("Metrics registered")
 
 	http.Handle("/metrics", promhttp.Handler())
+	//http.Handle("/snap")
+	http.HandleFunc("/snap", getSnap("192.168.20.162"))
 	log.Fatal(http.ListenAndServe(":"+metricsPort, nil))
 }
