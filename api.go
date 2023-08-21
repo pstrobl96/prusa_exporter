63a8825ddb6dd1c2e1e81b04aa750d4594b85f35
pstrobl96 <pavel.strobl@prusa3d.cz>
2023-08-21 15:12:56 +0200

    YOU SHALL NOT PASS! lol I suck. removed io/ioutil cause it's deprecated

--- api.go
+++ api.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
 	"time"
 
@@ -49,7 +49,7 @@ func accessBuddyApi(path string, address string, apiKey string, username string,
 		}
 	}
 	if err == nil {
-		body, err = ioutil.ReadAll(res.Body)
+		body, err = io.ReadAll(res.Body)
 		res.Body.Close()
 		if err != nil {
 			log.Error().Msg(err.Error())
@@ -73,7 +73,7 @@ func accessBuddyApi(path string, address string, apiKey string, username string,
 		return nil, err
 	} else {
 		defer res.Body.Close()
-		body, err := ioutil.ReadAll(res.Body)
+		body, err := io.ReadAll(res.Body)
 		if err != nil {
 			log.Error().Msg(err.Error())
 		}
@@ -92,7 +92,7 @@ func accessEinsyApi(path string, address string, apiKey string) ([]byte, error) 
 		return nil, err
 	} else {
 		defer res.Body.Close()
-		body, err := ioutil.ReadAll(res.Body)
+		body, err := io.ReadAll(res.Body)
 		if err != nil {
 			log.Error().Msg(err.Error())
 		}
