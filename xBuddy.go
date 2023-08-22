c5e76a2d6020ce8a5177546a69670a1b2286d16f
pstrobl96 <pavel.strobl@prusa3d.cz>
2023-08-22 10:16:42 +0200

    reformatting of xbuddy metrics

--- xBuddy.go
+++ xBuddy.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/rs/zerolog/log"
 )
@@ -22,58 +24,59 @@ type buddyCollector struct {
 }
 
 func newBuddyCollector() *buddyCollector {
+	defaultLabels := []string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"}
 	return &buddyCollector{
 		printerNozzleTemp: prometheus.NewDesc("prusa_buddy_nozzle_temperature",
 			"Current temperature of printer nozzle in Celsius",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerBedTemp: prometheus.NewDesc("prusa_buddy_bed_temperature",
 			"Current temperature of printer bed in Celsius",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerVersion: prometheus.NewDesc("prusa_buddy_version",
 			"Return information about printer. This metric contains information mostly about Prusa Link",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_api", "printer_server", "printer_text"},
+			append(defaultLabels, "printer_api", "printer_server", "printer_text"),
 			nil),
 		printerZHeight: prometheus.NewDesc("prusa_buddy_z_height",
 			"Current height of Z",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerPrintSpeed: prometheus.NewDesc("prusa_buddy_print_speed",
 			"Current setting of printer speed in percents (%)",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerTargetTempNozzle: prometheus.NewDesc("prusa_buddy_nozzle_target_temperature",
 			"Target temperature of printer nozzle in Celsius",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerTargetTempBed: prometheus.NewDesc("prusa_buddy_bed_target_temperature",
 			"Target temperature of printer bed in Celsius",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerFiles: prometheus.NewDesc("prusa_buddy_files",
 			"Number of files in storage",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_storage"},
+			append(defaultLabels, "printer_storage"),
 			nil),
 		printerPrintTimeRemaining: prometheus.NewDesc("prusa_buddy_printing_time_remaining",
 			"Returns time that remains for completion of current print",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerPrintProgress: prometheus.NewDesc("prusa_buddy_printing_progress",
 			"Returns information about completion of current print in percents",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerPrinting: prometheus.NewDesc("prusa_buddy_printing",
 			"Return information about printing",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 		printerMaterial: prometheus.NewDesc("prusa_buddy_material",
 			"Returns information about loaded filament. Returns 0 if there is no loaded filament",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path", "printer_filament"},
+			append(defaultLabels, "printer_filament"),
 			nil),
 		printerPrintTime: prometheus.NewDesc("prusa_buddy_print_time",
 			"Returns information about loaded filament. Returns 0 if there is no loaded filament",
-			[]string{"printer_address", "printer_model", "printer_name", "printer_job_name", "printer_job_path"},
+			defaultLabels,
 			nil),
 	}
 }
@@ -95,6 +98,14 @@ func (collector *buddyCollector) Describe(ch chan<- *prometheus.Desc) {
 
 }
 
+func BoolToFloat(boolean bool) float64 {
+	if boolean {
+		return 1.0
+	} else {
+		return 0.0
+	}
+}
+
 func getLabels(s buddy, job buddyJob, labelValues ...string) []string {
 	return append([]string{s.Address, s.Type, s.Name, job.Job.File.Name, job.Job.File.Path}, labelValues...)
 }
@@ -111,85 +122,48 @@ func (collector *buddyCollector) Collect(ch chan<- prometheus.Metric) {
 			if err != nil {
 				log.Error().Msg(err.Error())
 			} else {
-				bedTemp := prometheus.MustNewConstMetric(
-					collector.printerBedTemp, prometheus.GaugeValue,
-					float64(printer.Temperature.Bed.Actual),
-					getLabels(s,job)...)
-	
-				nozzleTemp := prometheus.MustNewConstMetric(
-					collector.printerNozzleTemp, prometheus.GaugeValue,
-					float64(printer.Temperature.Tool0.Actual),
-					getLabels(s,job)...)
-	
-				printProgress := prometheus.MustNewConstMetric(
-					collector.printerPrintProgress, prometheus.GaugeValue,
-					float64(job.Progress.Completion),
-					getLabels(s,job)...)
-	
-				printSpeed := prometheus.MustNewConstMetric(
-					collector.printerPrintSpeed, prometheus.GaugeValue,
-					float64(printer.Telemetry.PrintSpeed),
-					getLabels(s,job)...)
-	
-				printTimeRemaining := prometheus.MustNewConstMetric(
-					collector.printerPrintTimeRemaining, prometheus.GaugeValue,
-					float64(job.Progress.PrintTimeLeft),
-					getLabels(s,job)...)
-	
-				printingMetric := 0
-				if job.State == "Printing" {
-					printingMetric = 1
-				}
-	
-				printing := prometheus.MustNewConstMetric(
-					collector.printerPrinting, prometheus.GaugeValue,
-					float64(printingMetric),
-					getLabels(s,job)...)
-	
-				printTime := prometheus.MustNewConstMetric(
-					collector.printerPrintTime, prometheus.GaugeValue,
-					float64(job.Progress.PrintTime),
-					getLabels(s,job)...)
-	
-				targetTempBed := prometheus.MustNewConstMetric(
-					collector.printerTargetTempBed, prometheus.GaugeValue,
-					float64(printer.Temperature.Bed.Target),
-					getLabels(s,job)...)
-	
-				targetTempNozzle := prometheus.MustNewConstMetric(
-					collector.printerTargetTempNozzle, prometheus.GaugeValue,
-					float64(printer.Temperature.Tool0.Target),
-					getLabels(s,job)...)
-	
-				filamentLoaded := 0
-				if printer.Telemetry.Material != "---" {
-					filamentLoaded = 1
-				}
-	
-				material := prometheus.MustNewConstMetric(
-					collector.printerMaterial, prometheus.GaugeValue,
-					float64(filamentLoaded),
-					getLabels(s,job,printer.Telemetry.Material)...)
-	
-				printerVersion := prometheus.MustNewConstMetric(
-					collector.printerVersion, prometheus.GaugeValue,
-					1,
-					getLabels(s,job,printer.Telemetry.Material, version.API, version.Server, version.Text)...)
-	
+				bedTemp := prometheus.MustNewConstMetric(collector.printerBedTemp, prometheus.GaugeValue,
+					float64(printer.Temperature.Bed.Actual), getLabels(s, job)...)
+
+				nozzleTemp := prometheus.MustNewConstMetric(collector.printerNozzleTemp, prometheus.GaugeValue,
+					float64(printer.Temperature.Tool0.Actual), getLabels(s, job)...)
+
+				printProgress := prometheus.MustNewConstMetric(collector.printerPrintProgress, prometheus.GaugeValue,
+					float64(job.Progress.Completion), getLabels(s, job)...)
+
+				printSpeed := prometheus.MustNewConstMetric(collector.printerPrintSpeed, prometheus.GaugeValue,
+					float64(printer.Telemetry.PrintSpeed), getLabels(s, job)...)
+
+				printTimeRemaining := prometheus.MustNewConstMetric(collector.printerPrintTimeRemaining, prometheus.GaugeValue,
+					float64(job.Progress.PrintTimeLeft), getLabels(s, job)...)
+
+				printTime := prometheus.MustNewConstMetric(collector.printerPrintTime, prometheus.GaugeValue,
+					float64(job.Progress.PrintTime), getLabels(s, job)...)
+
+				targetTempBed := prometheus.MustNewConstMetric(collector.printerTargetTempBed, prometheus.GaugeValue,
+					float64(printer.Temperature.Bed.Target), getLabels(s, job)...)
+
+				targetTempNozzle := prometheus.MustNewConstMetric(collector.printerTargetTempNozzle, prometheus.GaugeValue,
+					float64(printer.Temperature.Tool0.Target), getLabels(s, job)...)
+
+				zHeight := prometheus.MustNewConstMetric(collector.printerZHeight, prometheus.GaugeValue,
+					printer.Telemetry.ZHeight, getLabels(s, job)...)
+
+				printing := prometheus.MustNewConstMetric(collector.printerPrinting, prometheus.GaugeValue,
+					float64(BoolToFloat(strings.Contains(job.State, "Printing"))), getLabels(s, job)...)
+
+				printerVersion := prometheus.MustNewConstMetric(collector.printerVersion, prometheus.GaugeValue,
+					1, getLabels(s, job, printer.Telemetry.Material, version.API, version.Server, version.Text)...)
+
+				material := prometheus.MustNewConstMetric(collector.printerMaterial, prometheus.GaugeValue,
+					float64(BoolToFloat(strings.Contains(printer.Telemetry.Material, "---"))), getLabels(s, job, printer.Telemetry.Material)...)
+
 				if len(files.Files) > 0 {
-					printerFiles := prometheus.MustNewConstMetric(
-						collector.printerFiles, prometheus.GaugeValue,
-						float64(len(files.Files[0].Children)),
-						getLabels(s,job,files.Files[0].Display)...
-					)
+					printerFiles := prometheus.MustNewConstMetric(collector.printerFiles, prometheus.GaugeValue,
+						float64(len(files.Files[0].Children)), getLabels(s, job, files.Files[0].Display)...)
 					ch <- printerFiles
 				}
-	
-				zHeight := prometheus.MustNewConstMetric(
-					collector.printerZHeight, prometheus.GaugeValue,
-					printer.Telemetry.ZHeight,
-					getLabels(s,job)...)
-	
+
 				ch <- bedTemp
 				ch <- nozzleTemp
 				ch <- printProgress
@@ -202,7 +176,7 @@ func (collector *buddyCollector) Collect(ch chan<- prometheus.Metric) {
 				ch <- material
 				ch <- printerVersion
 				ch <- zHeight
-			}	
+			}
 		}
 	}
 }
