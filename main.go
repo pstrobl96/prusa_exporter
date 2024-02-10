47c0b640ff16b488ceb3b3d98aea7f1706ff569d
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-02-10 17:26:55 +0100

    primitive syslog server is up and running

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func initProcedure() {
 }
 
 func main() {
+	go startSyslog(10008)
 	log.Info().Msg("Prusa exporter starting")
 	initProcedure()                          // initialize
 	if config.Exporter.ReloadInterval != 0 { // do not run reloader if interval is set to zero
---
+++ syslog.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/mcuadros/go-syslog.v2"
+)
+
+func startSyslog(port int) {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+	server.ListenUDP("0.0.0.0:" + fmt.Sprint(port))
+	server.Boot()
+
+	go func(channel syslog.LogPartsChannel) {
+		for logParts := range channel {
+			fmt.Println(logParts)
+		}
+	}(channel)
+
+	server.Wait()
+}
