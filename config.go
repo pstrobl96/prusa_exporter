80f9cc7100e344a72e7f327e26df6b25151df2f6
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-02-10 12:14:06 +0100

    update config.go and workaround for einsy boards connection test

--- config.go
+++ config.go
@@ -125,6 +125,14 @@ func probeConfigFile(parsedConfig configuration) configuration {
 			log.Error().Msg(s.Address + " is not reachable")
 		}
 	}
+	for i, s := range parsedConfig.Printers.Einsy {
+		if testConnection(s.Address) {
+			parsedConfig.Printers.Einsy[i].Reachable = true
+		} else {
+			parsedConfig.Printers.Einsy[i].Reachable = false
+			log.Error().Msg(s.Address + " is not reachable")
+		}
+	}
 	return parsedConfig
 }
 
@@ -132,6 +140,10 @@ func testConnection(s string) bool {
 	req, _ := http.NewRequest("GET", "http://"+s+"/", nil)
 	client := &http.Client{Timeout: time.Duration(config.Exporter.ScrapeTimeout) * time.Second}
 	r, e := client.Do(req)
+	if e == nil && r.StatusCode == 401 { // if the printer is password protected, it will return 401
+		log.Warn().Msg(s + " is password protected - there is no easy way to check if it is reachable at the moment")
+		return true
+	}
 	return e == nil && r.StatusCode == 200
 }
 
