fa597944baa9e608959497a51db01726fed3ab22
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-02-11 16:10:06 +0100

    fixed disabling syslog

--- main.go
+++ main.go
@@ -27,18 +27,21 @@ func main() {
 		go configReloader() // run reloader as goroutine
 	}
 
-	var syslogCollector *syslogCollector
 	if config.Exporter.SyslogMetrics {
 		log.Warn().Msg("Syslog metrics enabled!")
 		log.Warn().Msg("Syslog server starting at port: " + strconv.Itoa(config.Exporter.SyslogPort))
-		syslogCollector = newSyslogCollector()
 		go startSyslog(config.Exporter.SyslogPort)
 	}
 
 	log.Info().Msg("Initialized")
 	buddyCollector := newBuddyCollector()
 	einsyCollector := newEinsyCollector()
-	prometheus.MustRegister(buddyCollector, einsyCollector, syslogCollector)
+	if config.Exporter.SyslogMetrics {
+		syslogCollector := newSyslogCollector()
+		prometheus.MustRegister(buddyCollector, einsyCollector, syslogCollector)
+	} else {
+		prometheus.MustRegister(buddyCollector, einsyCollector)
+	}
 	log.Info().Msg("Metrics registered")
 	http.Handle("/metrics", promhttp.Handler())
 	log.Info().Msg("Listening at port: " + strconv.Itoa(config.Exporter.MetricsPort))
