6dceb5ab808289728dca4649695c9c43f0b2fe24
Pavel Strobl <pstrobl@proton.me>
2024-02-17 11:57:12 +0100

    created new main.go

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	configFile = kingpin.Flag("config.file", "Configuration file for prusa_exporter.").Default("config.yml").ExistingFile()
+)
+
+func main() {
+	log.Info().Msg("Prusa exporter starting")
+}
