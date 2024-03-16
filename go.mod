2b363d3f9cc52adc4105504b0f0e032557e9af80
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-03-16 20:31:24 +0100

    Updated network metrics

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/icholy/digest v0.1.22
 	github.com/prometheus/client_golang v1.18.0
 	github.com/rs/zerolog v1.32.0
+	github.com/stretchr/testify v1.8.2
 	gopkg.in/mcuadros/go-syslog.v2 v2.3.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v3 v3.0.1
@@ -16,11 +17,13 @@ require (
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
--- syslog/collector.go
+++ syslog/collector.go
@@ -1,6 +1,7 @@
 package syslog
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -121,15 +122,25 @@ func (collector *Collector) Collect(ch chan<- prometheus.Metric) {
 			case "esp_out":
 				fallthrough
 			case "eth_out":
-				valueKey = "sent"
-				labels = []string{splittedName[0]}
-				collectorItem = collector.printerNetworkOut
+				valueParsed, err = strconv.ParseFloat(v["sent"], 64)
+				if err != nil {
+					log.Error().Msgf("Error parsing value for metric %s: %s", k, err)
+					continue // Skip to next iteration if value parsing fails
+				}
+
+				ch <- prometheus.MustNewConstMetric(collector.printerNetworkOut, prometheus.CounterValue, valueParsed, getLabels(mac, ip, []string{splittedName[0]})...)
+				continue
 			case "esp_in":
 				fallthrough
 			case "eth_in":
-				valueKey = "recv"
-				labels = []string{splittedName[0]}
-				collectorItem = collector.printerNetworkIn
+				valueParsed, err = strconv.ParseFloat(v["recv"], 64)
+				if err != nil {
+					log.Error().Msgf("Error parsing value for metric %s: %s", k, err)
+					continue // Skip to next iteration if value parsing fails
+				}
+
+				ch <- prometheus.MustNewConstMetric(collector.printerNetworkIn, prometheus.CounterValue, valueParsed, getLabels(mac, ip, []string{splittedName[0]})...)
+				continue
 			case "24VVoltage":
 				fallthrough
 			case "5VVoltage":
@@ -237,8 +248,9 @@ func (collector *Collector) Collect(ch chan<- prometheus.Metric) {
 			case "adj_z":
 				collectorItem = collector.printerAxisZAdjustment
 			case "filament":
+				fmt.Println(v[valueKey])
 				valueParsed = 0
-				if v[valueKey] != "0" {
+				if v[valueKey] != "---" {
 					valueParsed = 1
 				}
 				ch <- prometheus.MustNewConstMetric(collector.printerFilament, prometheus.GaugeValue, valueParsed, getLabels(mac, ip, []string{v[valueKey]})...)
