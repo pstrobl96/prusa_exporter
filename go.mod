a688f6d17771680d603b7895e721d68154e3a38f
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2025-05-10 10:49:06 +0200

    Syslog server

--- go.mod
+++ go.mod
@@ -25,4 +25,5 @@ require (
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	google.golang.org/protobuf v1.36.4 // indirect
+	gopkg.in/mcuadros/go-syslog.v2 v2.3.0 // indirect
 )
--- lineprotocol/syslog.go
+++ lineprotocol/syslog.go
@@ -1,1 +1,33 @@
 package lineprotocol
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"gopkg.in/mcuadros/go-syslog.v2"
+)
+
+func startSyslogServer(listenUDP string) (syslog.LogPartsChannel, *syslog.Server) {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+	server.ListenUDP(listenUDP)
+	server.Boot()
+	return channel, server
+}
+
+// MetricsListener is a function to handle syslog metrics and sent them to processor
+func MetricsListener(listenUDP string, influxURL string, influxToken string, influxBucket string, influxOrg string, prefix string) {
+	channel, server := startSyslogServer(listenUDP)
+
+	go func(channel syslog.LogPartsChannel) {
+		for logParts := range channel {
+			log.Trace().Msg(fmt.Sprintf("%v", logParts))
+		}
+	}(channel)
+
+	server.Wait()
+
+}
